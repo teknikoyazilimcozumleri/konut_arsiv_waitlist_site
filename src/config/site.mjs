c5b0c545bcb4163a35a06ef41ebc90129a4f32d3
nsgorlew <nsgorlew@gmail.com>
2026-05-05 14:41:16 +0300

    bug fixes

--- src/config/site.mjs
+++ src/config/site.mjs
@@ -1,10 +1,10 @@
 export const SITE = {
   title: "KonutArşiv",
   description: "",
-  defaultLanguage: "en",
+  defaultLanguage: "tr",
   url: "https://konutarsiv.com", // Update this with your actual site URL
   author: "Tekniko Yazılım Çözümleri",
-  ogImage: "/og-image.png", // Add this image to your public folder
+  ogImage: "/konutarsivlogo.png", // Add this image to your public folder
   downloadUrl: "https://play.google.com/store/apps",
   iosDownloadUrl: "https://apps.apple.com/",
   androidDownloadUrl: "https://play.google.com/store/apps"
